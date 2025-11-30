1ed3185595868a5b5f9020ed04cfe9cb54309d14
Vitor Castello <vitorcastello@Vitors-MacBook-Pro.local>
2025-11-30 13:41:33 -0300

    Update produtos backfill cron to 15 minutes

---
+++ supabase/migrations/20251202112000_adjust_tiny_produtos_cron_15min.sql
@@ -0,0 +1,20 @@
+-- Ajusta tiny_produtos_backfill_hourly para rodar a cada 15 minutos (*/15 * * * *).
+set search_path = public, extensions;
+
+do $$
+declare
+  v_jobid int;
+begin
+  select jobid
+  into v_jobid
+  from cron.job
+  where jobname = 'tiny_produtos_backfill_hourly';
+
+  if v_jobid is not null then
+    perform cron.alter_job(
+      jobid    => v_jobid,
+      schedule => '*/15 * * * *'
+    );
+  end if;
+end;
+$$;
